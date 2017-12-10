3316fe746113003efc1650f750001c447d0abf76
Mark <mamjong@avans.nl>
2017-12-10 23:47:04 +0100

    Endpoint for preflight request.
    Added response for preflight request if all access-control succeeds.

--- app.js
+++ app.js
@@ -25,10 +25,15 @@ app.set('env', (process.env.ENV || 'development'));
 
 app.use(function (req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN || 'http://localhost:4200');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 	res.setHeader('Access-Control-Allow-Credentials', true);
-	next();
+
+	if (req.method === 'OPTIONS') {
+		return res.status(200).end();
+	}
+
+	return next();
 });
 
 routes(app);
